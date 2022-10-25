e187492ab3f0b00e0ba8e0d6db1ec79905fb5447
pandaoscar <81119860+pandaoscar@users.noreply.github.com>
2022-10-24 19:46:10 -0500

    Activitys Login SignuP

--- app/src/main/java/com/example/tuempleo/MainActivity.java
+++ app/src/main/java/com/example/tuempleo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.tuempleo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
@@ -66,4 +67,5 @@ public class MainActivity extends AppCompat {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
 }
--- app/src/main/java/com/example/tuempleo/fragment_about.java
+++ app/src/main/java/com/example/tuempleo/fragment_about.java
@@ -1,5 +1,6 @@
 package com.example.tuempleo;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -61,4 +62,5 @@ public class fragment_about extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about, container, false);
     }
+
 }
