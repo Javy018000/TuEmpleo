b34b40dbbdc1be51e4d5e485da5ba882062aece3
pandaoscar <81119860+pandaoscar@users.noreply.github.com>
2022-10-24 22:39:23 -0500

    Intento Final Login SignuP

--- app/src/main/java/com/example/tuempleo/LogInActivity.java
+++ app/src/main/java/com/example/tuempleo/LogInActivity.java
@@ -1,14 +1,69 @@
 package com.example.tuempleo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LogInActivity extends AppCompatActivity {
-
+    private EditText email;
+    private EditText passw;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        email=findViewById(R.id.li_userName);
+        passw=findViewById(R.id.li_passw);
+        mAuth = FirebaseAuth.getInstance();
     }
-}
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+
+
+    public void logIn(View view){
+        mAuth.signInWithEmailAndPassword(email.getText().toString().trim(),passw.getText().toString().trim())
+                .addOnCompleteListener(this,new OnCompleteListener<AuthResult>(){
+
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            //Log.d(TAG,"Correcto");
+                            Toast.makeText(getApplicationContext(),"Acceso Concedido",Toast.LENGTH_SHORT).show();
+                            FirebaseUser user=mAuth.getCurrentUser();
+
+                            Intent intent= new Intent(getApplicationContext(),TrActivity.class);
+                            startActivity(intent);
+                            //updateUI(user);
+                        }else{
+                            //Log.w(TAG,"",task.getExeption());
+                            Toast.makeText(getApplicationContext(),"Autotentificacion no se pudo",Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
+
+
+        }
+
+    }
--- app/src/main/java/com/example/tuempleo/SignUpActivity.java
+++ app/src/main/java/com/example/tuempleo/SignUpActivity.java
@@ -1,14 +1,69 @@
 package com.example.tuempleo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText email;
+    private EditText passw;
+    private EditText comPassw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        mAuth = FirebaseAuth.getInstance();
+        email= findViewById(R.id.userName);
+        passw= findViewById(R.id.passw);
+        comPassw= findViewById(R.id.confirPassw);
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    public void sigInUser(View view){
+
+        if(passw.getText().toString().trim().equals(comPassw.getText().toString().trim())){
+
+            mAuth.createUserWithEmailAndPassword(email.getText().toString(),passw.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(), "Usuario Creado.", Toast.LENGTH_SHORT).show();
+                                // Sign in success, update UI with the signed-in user's information
+                                //Log.d(TAG, "signInWithCustomToken:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Intent intent= new Intent(getApplicationContext(),MainActivity.class);
+                                startActivity(intent);
+                                //updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                //Log.w(TAG, "signInWithCustomToken:failure", task.getException());
+                                Toast.makeText(getApplicationContext(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                //updateUI(null);
+                            }
+                        }
+                    });
+        }else{
+            Toast.makeText(this,"ERROR",Toast.LENGTH_SHORT).show();
+        }
     }
 }
